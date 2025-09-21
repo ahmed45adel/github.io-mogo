5d1d1fa3871e88f3b82728336bf4025ef4272299
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-21 10:39:48 +0300

    remove ar en

--- script/scipt.js
+++ script/scipt.js
@@ -1,28 +1,3 @@
-changeLang = () => {
-
-   let html = document.querySelector("html")
-   let link = document.createElement('link');
-   let language = html.getAttribute("lang");
-   let head = document.getElementsByTagName('HEAD')[0]
-   if (language === "en") {
-
-      html.setAttribute("lang", "ar");
-      html.setAttribute("dir", "rtl");
-
-      link.rel = 'stylesheet';
-      link.href = './css/RTL.css';
-      head.appendChild(link);
-   }
-   else {
-      html.setAttribute("lang", "en");
-      html.setAttribute("dir", "ltr");
-      head.removeChild(head.lastChild);
-
-   }
-   ;
-
-}
-
 hideWelcome = () => {
    let hero = document.querySelector(".hero");
    if (hero.style.display !== "none") {
